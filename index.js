201715b14ca23fda147015340bbd62b3c4215870
contra <yo@contra.io>
2020-10-30 12:21:36 -0700

    feat: rewrite, real streams

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-var Excel = require('exceljs')
-var through = require('through2')
-var duplex = require('duplexify')
+const Excel = require('exceljs')
+const through = require('through2')
+const duplex = require('duplexify')
+const { pipeline, Readable } = require('readable-stream')
 
-var defaultOpts = {
-  objectMode: true
-}
-
-module.exports = function exceljsStream(opts) {
-  opts = Object.assign(defaultOpts, opts)
-  var input = through()
-  var second = through({ objectMode: opts.objectMode })
-  var workbook = new Excel.Workbook()
-
-  var headers = null
-  var reader = workbook.xlsx.read(input)
-  .then(function (worksheet) {
-    workbook.eachSheet(function (sheet, id) {
-      sheet.eachRow(function (row, id) {
-        if (id === 1 || !headers) {
-          headers = opts.mapHeaders ? row.values.map(opts.mapHeaders) : row.values
-          return
-        }
-        var item = {}
-        row.values.forEach(function (v, k) {
-           if (!headers) return
-          item[headers[k]] = opts.mapValues ? opts.mapValues(v) : v
-        })
-        if (!opts.objectMode) {
-          second.push(JSON.stringify(item))
-          return
-        }
-        second.push(item)
-      })
-    })
-    second.end()
+module.exports = function excelStream(opts={}) {
+  const input = through()
+  const reader = new Excel.stream.xlsx.WorkbookReader(input, {
+  	entries: 'emit',
+  	sharedStrings: 'cache',
+  	hyperlinks: 'cache',
+  	styles: 'cache',
+  	worksheets: 'emit'
   })
-    .catch((err) => {
-      if (err.message && err.message.indexOf('is this a zip') !== -1) {
+  const createReader = async function* () {
+    for await (const worksheet of reader) {
+      for await (const row of worksheet) {
+        yield row
+      }
+    }
+  }
+  
+  let headers
+  const out = pipeline(
+    Readable.from(createReader()),
+    through.obj(function (row, _, cb) {
+      if (row.values.length === 0) return cb() // blank
+      if (!headers) {
+        headers = opts.mapHeaders ? row.values.map(opts.mapHeaders) : row.values
+        out.emit('header', headers)
+        return cb()
+      }
+      const item = row.values.reduce((acc, v, idx) => {
+        acc[headers[idx]] = opts.mapValues ? opts.mapValues(v) : v
+        return acc
+      }, {})
+      cb(null, item)
+    }),
+    (err) => {
+      if (!err) return
+      if (err.message && err.message.indexOf('invalid signature') !== -1) {
         err = new Error('Legacy XLS files are not supported, use an XLSX file instead!')
       }
-      second.emit('error', err)
-    })
-  return duplex.obj(input, second)
+      out.emit('error', err)
+    }
+  )
+  return duplex.obj(input, out)
 }
--- test/index.js
+++ test/index.js
@@ -1,56 +1,39 @@
 var fs = require('fs')
 var should = require('should')
-var stream = require('stream')
+var stream = require('readable-stream')
+var collect = require('get-stream')
 var exs = require('..')
 
 describe('exceljs-through-stream', function () {
-  it('should export a function', function (done) {
+  it('should export a function', function () {
     should(typeof exs).equal('function')
-    done()
   })
-  it('should export a function that returns a stream', function (done) {
+  it('should export a function that returns a stream', function () {
     should(exs() instanceof stream)
-    done()
   })
-  it('parse xlsx files', function (done) {
-    var file = fs.createReadStream(__dirname + '/file.xlsx')
-    var res = []
-    file.pipe(exs())
-      .on('data', function (d) {
-        res.push(d)
-      })
-      .on('end', function () {
-        should(res.length).equal(4)
-        should(JSON.stringify(res[0])).equal(
-        JSON.stringify({
-          row: 'row1',
-          date: '2017-02-08T00:00:00.000Z',
-          cost: 100,
-          notes: 111
-        }))
-        done()
-      })
+  it('parse xlsx files', async () => {
+    const file = fs.createReadStream(__dirname + '/file.xlsx')
+    const res = await collect.array(file.pipe(exs()))
+    should(res.length).equal(4)
+    should(res[0]).eql({
+      row: 'row1',
+      date: new Date('2017-02-08T00:00:00.000Z'),
+      cost: 100,
+      notes: 111
+    })
   })
-  it('parse xlsx files with string mode', function (done) {
-    var file = fs.createReadStream(__dirname + '/file.xlsx')
-    var res = []
-    file.pipe(exs({ objectMode: false }))
-      .on('data', function (d) {
-        res.push(JSON.parse(d))
-      })
-      .on('end', function () {
-        should(res.length).equal(4)
-        should(JSON.stringify(res[0])).equal(
-        JSON.stringify({
-          row: 'row1',
-          date: '2017-02-08T00:00:00.000Z',
-          cost: 100,
-          notes: 111
-        }))
-        done()
-      })
+  it('parse larger xlsx files', async () => {
+    const file = fs.createReadStream(__dirname + '/larger-file.xlsx')
+    const res = await collect.array(file.pipe(exs()))
+    should(res.length).eql(51000)
+    should(res[0]).eql({
+      Department: 'Law Enforcement',
+      'Fiscal Year': '2017-18',
+      'Property Value': 5000,
+      'Tax Dollars': 4.74
+    })
   })
-  it('return error if file is invalid', function (done) {
+  it('return error if file is invalid', (done) => {
     var file = fs.createReadStream(__dirname + '/index.js')
     file.pipe(exs({ objectMode: false }))
       .on('error', function (e) {
